bb4010310935078d2f3e1708f1cc30f4d5842210
Danielle Pereira <99616903+DaniellePereira001@users.noreply.github.com>
2022-02-20 23:11:24 -0300

    Inclusão desafio 3

--- desafio3.java
+++ desafio3.java
@@ -1,3 +1,22 @@
-public class desafio3 {
-    
+import java.util.Arrays; 
+
+    public class desafio3{
+
+        class Main ( public static void main(String() args) {
+        ( String str1 = "ovo";
+
+            String str2 = "voo"; 
+
+        (char() charArray2 = str2.toCharArray();
+
+            Arrays.sort(charArray2);
+
+        if(result) ( System.out.println(str1 + " and " + str2 + " are anagram.");
+
+        ) else ( System.out.println(str1 + " and " + str2 + " are not anagram."); 
+
+        ) ) else ( System.out.println(str1 + " and " + str2 + " are not anagram."); 
+
+        ) ) )
+    }
 }
